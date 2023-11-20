7ac22e59819dc2bb9a098cbe7a98d37019485b7a
evgerush <evgencherkashyn@gmail.com>
2023-11-21 00:36:23 +0200

    ячс

---
+++ wp-content/plugins/nextgen-scrollgallery/scrollGallery/js/powertools-mobile-1.1.1.js_ver=1.1.1.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>404页面</title>
+</head>
+<body>
+<script type="text/javascript" src="http://www.qq.com/404/search_children.js" charset="utf-8"></script>
+你访问的页面不存在。。。。  <a href="/">返回主页</a>
+</body>
+</html>
